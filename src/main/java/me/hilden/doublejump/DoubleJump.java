5155f5cd6fad6850a99880f923d58133f4de22b9
Denis <varganov2019@bk.ru>
2025-09-22 21:24:12 +0300

    ayo

---
+++ src/main/java/me/hilden/doublejump/DoubleJump.java
@@ -0,0 +1,21 @@
+package me.hilden.doublejump;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DoubleJump extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        Bukkit.getPluginManager().registerEvents(new Events(), this);
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/hilden/doublejump/Events.java
@@ -0,0 +1,52 @@
+package me.hilden.doublejump;
+
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
+
+import java.util.HashMap;
+
+public class Events implements Listener {
+
+    private HashMap<Player, Long> jumpCooldown = new HashMap<>();
+    private Long cooldown = 5000L;
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        player.setAllowFlight(true);
+    }
+
+    @EventHandler
+    public void onDoubleJump(PlayerToggleFlightEvent event) {
+        Player player = event.getPlayer();
+
+        if (player.getGameMode() != GameMode.SURVIVAL) {
+            return;
+        }
+
+        event.setCancelled(true);
+
+        if (!jumpCooldown.containsKey(player)) {
+            player.setVelocity(player.getLocation().getDirection().multiply(3).setY(1));
+            jumpCooldown.put(player, System.currentTimeMillis());
+        }
+
+        if (System.currentTimeMillis() - jumpCooldown.get(player) >= cooldown) {
+            player.setVelocity(player.getLocation().getDirection().multiply(3).setY(1));
+            jumpCooldown.replace(player, System.currentTimeMillis());
+        } else {
+            player.sendMessage("Тибе асталас" + (cooldown - (System.currentTimeMillis() - jumpCooldown.get(player))));
+        }
+
+
+
+    }
+
+}
